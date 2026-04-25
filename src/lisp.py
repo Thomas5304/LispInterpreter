a53416e66160c4169eba9b0a6a95ce2083dbef41
Thomas Dilling <thomas.dilling@me.com>
2026-04-25 06:45:46 +0200

    Just formating

--- src/lisp.py
+++ src/lisp.py
@@ -137,38 +137,38 @@ class Env:
     def __init__(self, parent = None):
         self.data = {}
         self.parent = parent
-        
+
     def get(self, name):
         if name == "t":
             return True
-            
+
         if name == "nil":
             return None
-            
+
         if name in self.data:
             return self.data[name]
         if self.parent is not None:
             return self.parent.get(name)
         raise NameError(f"unbound symbol: {name}  -- {self}")
-    
+
     def set(self, name, value):
         self.data[name] = value
-        
+
     def __str__(self):
         ret = ""
         if self.parent is not None:
             ret += str(self.parent)
         return ret + str(self.data)
-        
+
 class FunctionDef:
     def __init__(self, closure, params, body) -> None:
         self.closure = closure
         self.params = params
-        self.body = body     
-     
-        
+        self.body = body
+
+
 class LispInterpreter:
-        
+
     def __init__(self, lisp_tree : list[Any]):
         self.lisp_tree = lisp_tree
         self.specialforms = {
@@ -197,27 +197,27 @@ class LispInterpreter:
         #print(expression)
         if isinstance(expression, str):
             new_expression = env.get(expression)
-            
+
             return new_expression
-            
+
         if isinstance(expression, (float,int)):
             return expression
 
         if not isinstance(expression, list):
             raise ValueError(f"invalid value {expression}")
 
-        function = expression[0] 
+        function = expression[0]
         args = expression[1:]
-        
+
         if isinstance(function, list):
             function = self.run_rec(env, function)
 
         if function in self.specialforms:
             return self.specialforms[function](env, args)
-        
+
 
         values = [self.run_rec(env, arg) for arg in args]
-        
+
         if isinstance(function, (int, float)):
             return [function, *values]
 
@@ -232,9 +232,9 @@ class LispInterpreter:
                 new_env = Env(func.closure)
                 for name, value in zip(func.params, values):
                     new_env.set(name, value)
-                    
+
                 return self.run_rec(new_env, func.body)
-            else: 
+            else:
                 raise ValueError(f"unknown function {function}")
 
     def run(self):
@@ -243,17 +243,17 @@ class LispInterpreter:
         for e in self.lisp_tree:
             ret = self.run_rec(env, e)
         return ret
-                
+
     def create_lambda(self, env, args):
         params, body = args
         return FunctionDef(env, params, body)
-        
+
     def define_function(self, env, args):
         name, params, body = args
         func = FunctionDef(env, params, body)
         env.set(name, func)
-        
-        
+
+
     def begin(self, env, args):
         _, expressions = args
         ret = None
@@ -263,78 +263,78 @@ class LispInterpreter:
 
     def ifthenelse(self, env, args):
         condition, true_branch, false_branch = args
-        cond_result = self.run_rec(env, condition) 
+        cond_result = self.run_rec(env, condition)
         #print(f"{cond_result=}")
         if cond_result is not None:
             return self.run_rec(env, true_branch)
         else:
             return self.run_rec(env, false_branch)
-            
+
     def define(self, env: Env, args):
         var, value = args
         env.set(var, self.run_rec(env, value))
-        
+
     def let(self, env: Env, args):
         env = Env(env)
         vars, *expressions = args
-        
+
         for var in vars:
             self.define(env, var)
-            
+
         ret = None
-        
+
         for expression in expressions:
             ret = self.run_rec(env, expression)
-            
+
         env = env.parent
-        
+
         return ret
-    
+
     def create_list(self, args):
         return list(args)
-    
+
     def map(self, env, args):
         func, values = args
         return [self.run_rec(env, [func, value]) for value in values]
-        
+
     def greater(self, args):
         a, b = args
         #print(f"{a=} > {b=}")
         ret = True if a>b else None
         #print(f"{ret=}")
         return ret
-        
+
     def greaterequal(self, args):
         a, b = args
         #print(f"{a=} >= {b=}")
         ret = True if a>=b else None
         #print(f"{ret=}")
         return ret
-        
+
     def less(self, args):
         a, b = args
         #print(f"{a=} < {b=}")
         ret = True if a<b else None
         #print(f"{ret=}")
         return ret
-        
+
     def lessequal(self, args):
         a, b = args
         #print(f"{a=} <= {b=}")
         ret = True if a<=b else None
         #print(f"{ret=}")
         return ret
-        
+
     def equal(self, args):
         a, b = args
         #print(f"{a=} == {b=}")
         ret = True if a==b else None
         #print(f"{ret=}")
         return ret
-        
+
     def add(self, args):
         return sum(args)
-        
+
     def mult(self, args):
         result = 1
         for arg in args:
@@ -351,7 +351,7 @@ class LispInterpreter:
         for arg in args[1:]:
             result -= arg
         return result
-        
+
     def div(self, args):
         l = len(args)
         if l == 0:
@@ -381,7 +381,7 @@ def main() -> None:
         )
     )
     (list (let ((a 2) (b 3 )) (+ 1 a b (* 2 2) (if nil (/ 2 1) (+ 3 2))) (+ a b)) 10)"""
-    
+
     lisp_lines4 = """\
     (define a 10)
     (defun test (m) (lambda (x) (* m x)))
@@ -398,7 +398,7 @@ def main() -> None:
     """
     lisp_lines5 = """\
     (defun fib (x)
-        (if (>= x 3) 
+        (if (>= x 3)
             (+ (fib (- x 1)) (fib (- x 2)))
             1
         )
@@ -415,13 +415,13 @@ def main() -> None:
 ((lambda (x) (* 3 x)) 3)
     """
     parser : LispParser = LispParser(lisp = LispCtx())
-    
+
     parsed_lisp = parser.parse(tokenize(lisp_lines5))
     print(f"{parsed_lisp=}")
-    
+
     interpreter = LispInterpreter(parsed_lisp)
     print(f"interpreter.run({parsed_lisp})={interpreter.run()}")
-    
+
     exit(0)
 
     ctx = LispCtx()
