2598ced457fcecf8a10387686f0e42d5a62fd917
Thomas Dilling <thomas.dilling@me.com>
2026-05-03 10:49:27 +0200

    Testcases and ignore pychache

--- src/lispfile.lisp
+++ src/lispfile.lisp
@@ -1,1 +1,31 @@
 (defun cadr (lst) (car (cdr lst)))
+
+(defmacro layer-definition (&rest clauses)
+
+  `(register-layer
+      ,@(apply #'append
+          (mapcar
+            (lambda (clause)
+
+              (let ((key (car clause))
+                    (value (cadr clause)))
+
+                (list
+                  (intern
+                    (concatenate 'string ":" (symbol-name key)))
+                  `',value)))
+
+            clauses))))
+
+
+(defun register-layer (&rest specs) (print-eval specs))
+
+;(defmacro layer-definition (&rest clauses)
+;
+;  `(register-layer
+;     ,clauses))
+
+
+
+(defun plus2 (x) (+ 2 x))
+(defun multadd2 (&keys m a) (* m (+ a 2)))
