8cba471281daa795cb801dee835c36eef175fd0f
Thomas Dilling <thomas.dilling@me.com>
2026-05-04 09:55:54 +0200

    Test cases for DSL

--- src/lispfile.lisp
+++ src/lispfile.lisp
@@ -1,31 +1,9 @@
 (defun cadr (lst) (car (cdr lst)))
-
-(defmacro layer-definition (&rest clauses)
-
-  `(register-layer
-      ,@(apply #'append
-          (mapcar
-            (lambda (clause)
-
-              (let ((key (car clause))
-                    (value (cadr clause)))
-
-                (list
-                  (intern
-                    (concatenate 'string ":" (symbol-name key)))
-                  `',value)))
-
-            clauses))))
-
-
-(defun register-layer (&rest specs) (print-eval specs))
-
-;(defmacro layer-definition (&rest clauses)
-;
-;  `(register-layer
-;     ,clauses))
-
-
-
-(defun plus2 (x) (+ 2 x))
-(defun multadd2 (&keys m a) (* m (+ a 2)))
+(defun concatenate (&rest args) (+ args))
+(defun length (lst)
+    (if
+        (null? lst)
+            0
+            (length (cdr lst))
+        )
+    )
