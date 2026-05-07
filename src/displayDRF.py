17d3210d0d47984be06d6f4baff67452f758a1e6
Dilling Thomas <thomas.dilling@tdk.com>
2026-05-07 07:21:15 +0200

    Printing stipples with python function

--- src/displayDRF.py
+++ src/displayDRF.py
@@ -2,4 +2,9 @@ import os
 import pathlib
 
 def printStipple(name, display, stipple):
-    print(f"Stipple {display}/{name}\n")
+    print(f"Stipple {display}/{name}\n{stipple}")
+    for row in stipple:
+        for col in row:
+            print("\u2588" if col else " ", end="")
+        print()
+
