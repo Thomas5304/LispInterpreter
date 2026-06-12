be32c95e018f014a1d533a6fdbab65d192b97f78
Thomas Dilling <thomas.dilling@me.com>
2026-06-12 19:25:30 +0200

    Improving default lisp file

--- src/lispfile.lisp
+++ src/lispfile.lisp
@@ -1,5 +1,12 @@
+
+(defun 1+ (var) (begin (+ var 1)))
+(defun 1- (var) (begin (- var 1)))
+
+(defmacro incr (var &optional (diff 1)) `(begin (set! ,var (+ ,var ,diff)) ,var))
+(defmacro decr (var &optional (diff 1)) `(begin (set! ,var (- ,var ,diff)) ,var))
+
 (defun cadr (lst) (car (cdr lst)))
-(defun nth (n lst) (if (<= n 0) (car lst) (nth (- n 1) (cdr lst))))
+(defun nth (n lst) (if (<= n 0) (car lst) (nth (1- n) (cdr lst))))
 (defun concatenate (&rest args) (+ args))
 (defun length (lst)
     (if
@@ -53,23 +60,19 @@
 (defun dont-quit () (set! __.QUIT.__ nil))
 
 
-(defun 1+ (var) (begin (+ var 1)))
-(defun 1- (var) (begin (- var 1)))
-
-(defmacro incr (var) `(begin (set! ,var (1+ ,var)) ,var))
-(defmacro decr (var) `(begin (set! ,var (1- ,var)) ,var))
 
 (defmacro mk-counter (name &key (start 0) (increment 1))
-  (let ((pre-plus (intern (format "{}+" name)))    ;; Erzeugt z.B. zähler-1+
-        (pre-minus (intern (format "{}-" name)))   ;; Erzeugt z.B. zähler-1-
+  (let ((pre-plus (intern (format "++{}" name)))    ;; Erzeugt z.B. zähler-1+
+        (pre-minus (intern (format "--{}" name)))   ;; Erzeugt z.B. zähler-1-
         (post-plus (intern (format "{}++" name)))    ;; Erzeugt z.B. zähler-1+
         (post-minus (intern (format "{}--" name)))   ;; Erzeugt z.B. zähler-1-
-        (get (intern (format "get-{}" name)))) ;; Erzeugt z.B. get-zähler-1
+        (get (intern (format "get-{}" name))) ;; Erzeugt z.B. get-zähler-1
+        (values (intern (format "values-{}" name))))
     `(let ((counter ,start))
-       (defun ,pre-plus () (begin (set! counter (+ counter ,increment)) counter))
-       (defun ,pre-minus () (begin (set! counter (- counter ,increment)) counter))
-       (defun ,post-plus () (let ((old counter)) (set! counter
-         (+ counter ,increment)) old))
-       (defun ,post-minus () (let ((old counter)) (set! counter
-         (- counter ,increment)) old))
+       (defun ,pre-plus () (begin (incr counter ,increment) counter))
+       (defun ,pre-minus () (begin (decr counter ,increment) counter))
+       (defun ,post-plus () (let ((old counter)) (incr counter ,increment) old))
+       (defun ,post-minus () (let ((old counter)) (decr counter ,increment) old))
+       ;(defun ,name () (list ',name "value" counter "start" ,start "increment" ,increment))
+       (defun ,values () (list ',name counter ,start ,increment))
        (defun ,get () counter))))
